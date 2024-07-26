b417e433b223c122d1b521c5e4cbe390cdd883c0
Dmitriy <chupa5598@gmail.com>
2024-07-26 20:06:19 +0300

    Инициализация VK-бота

--- src/vk_bot/config.py
+++ src/vk_bot/config.py
@@ -1,12 +1,25 @@
-from pydantic_settings import BaseSettings
+from pathlib import Path
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+from vkbottle import API, BuiltinStateDispenser
+from vkbottle.bot import BotLabeler
+
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 
 
 class Settings(BaseSettings):
     database_url: str
     tg_token: str
+    vk_token: str
 
     class Config:
-        env_file = '.env'
+        env_file = env_path
 
 
 settings = Settings()
+
+api = API(settings.vk_token)
+labeler = BotLabeler()
+state_dispenser = BuiltinStateDispenser()
