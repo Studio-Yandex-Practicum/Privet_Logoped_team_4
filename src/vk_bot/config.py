0ff76498bd471324f772d3b155bbe7a04047aa70
Dmitriy <chupa5598@gmail.com>
2024-07-26 21:15:06 +0300

    Использование environs вместо pydantic

--- src/vk_bot/config.py
+++ src/vk_bot/config.py
@@ -1,25 +1,12 @@
-from pathlib import Path
-
-from dotenv import load_dotenv
-from pydantic import BaseSettings
 from vkbottle import API, BuiltinStateDispenser
 from vkbottle.bot import BotLabeler
+from environs import Env
 
-env_path = Path(__file__).resolve().parent.parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
+env = Env()
+env.read_env()
 
+vk_token = env('VK_TOKEN')
 
-class Settings(BaseSettings):
-    database_url: str
-    tg_token: str
-    vk_token: str
-
-    class Config:
-        env_file = env_path
-
-
-settings = Settings()
-
-api = API(settings.vk_token)
+api = API(vk_token)
 labeler = BotLabeler()
 state_dispenser = BuiltinStateDispenser()
