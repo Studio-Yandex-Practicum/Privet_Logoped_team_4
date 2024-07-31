db0587d1f8b85e0f4a6062f34c2a373f65abf76f
Dmitriy Pavlov <chupa5598@gmail.com>
2024-07-31 12:40:16 +0300

    Поправлены добавление пользователя в бд и изменение роли.

--- src/vk_bot/handlers/role_handler.py
+++ src/vk_bot/handlers/role_handler.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from sqlalchemy import select, update
+from sqlalchemy.dialects.postgresql import insert
 
 parent_folder_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../..')
@@ -14,49 +14,31 @@ from db.models import RoleType, VKUser, async_session  # noqa
 
 
 async def role_handler(bot, message, UserStates):
-    if message.text.lower() == 'родитель':
+    if message.text.lower() == 'родитель' or message.text.lower() == 'логопед':
+        if message.text.lower() == 'родитель':
+            role = RoleType.PARENT
+        else:
+            role = RoleType.SPEECH_THERAPIST
         user_info = await message.get_user()
         async with async_session() as session:
-            result = await session.execute(
-                select(VKUser).where(VKUser.user_id == user_info.id)
+            new_user = insert(VKUser).values(
+                user_id=user_info.id, role=role
+                ).on_conflict_do_update(
+                constraint=VKUser.__table__.primary_key,
+                set_={VKUser.role: role}
             )
-            user = result.scalars().first()
-            if not user:
-                new_user = VKUser(user_id=user_info.id, role=RoleType.PARENT)
-                session.add(new_user)
-                await session.commit()
-            elif user and user.role != RoleType.PARENT:
-                await session.execute(
-                    update(VKUser).where(VKUser.user_id == user_info.id).
-                    values(role=RoleType.PARENT)
-                )
-                await session.commit()
-        await message.answer('Вы выбрали роль Родитель. Вот ваши опции:',
-                             keyboard=parent_keyboard)
-        await bot.state_dispenser.set(
-            message.peer_id, UserStates.PARENT_STATE)
-    elif message.text.lower() == 'логопед':
-        user_info = await message.get_user()
-        async with async_session() as session:
-            result = await session.execute(
-                select(VKUser).where(VKUser.user_id == user_info.id)
-            )
-            user = result.scalars().first()
-            if not user:
-                new_user = VKUser(user_id=user_info.id,
-                                  role=RoleType.SPEECH_THERAPIST)
-                session.add(new_user)
-                await session.commit()
-            elif user and user.role != RoleType.SPEECH_THERAPIST:
-                await session.execute(
-                    update(VKUser).where(VKUser.user_id == user_info.id).
-                    values(role=RoleType.SPEECH_THERAPIST)
-                )
-                await session.commit()
-        await message.answer('Вы выбрали роль Логопед. Вот ваши опции:',
-                             keyboard=speech_therapist_keyboard)
-        await bot.state_dispenser.set(
-            message.peer_id, UserStates.SPEECH_THERAPIST_STATE)
+            await session.execute(new_user)
+            await session.commit()
+        if message.text.lower() == 'родитель':
+            await message.answer('Вы выбрали роль Родитель. Вот ваши опции:',
+                                 keyboard=parent_keyboard)
+            await bot.state_dispenser.set(
+                message.peer_id, UserStates.PARENT_STATE)
+        else:
+            await message.answer('Вы выбрали роль Логопед. Вот ваши опции:',
+                                 keyboard=speech_therapist_keyboard)
+            await bot.state_dispenser.set(
+                message.peer_id, UserStates.SPEECH_THERAPIST_STATE)
     else:
         await message.answer(
             'Пожалуйста, выберите одну из предложенных ролей:',
