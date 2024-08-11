b7c756bd9b2e9110e1707d62fee3298482b4b638
Dmitriy <chupa5598@gmail.com>
2024-08-11 20:18:39 +0300

    Блокировка вк пользователей через мидлвари

--- src/vk_bot/main.py
+++ src/vk_bot/main.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import logging
 
 from config import api, labeler, state_dispenser
@@ -13,18 +11,14 @@ from handlers import (add_link, add_promocode, admin_handler,
                       start_handler, unban_user)
 from vkbottle import BaseStateGroup
 from vkbottle.bot import Bot, Message
-from sqlalchemy import select
-
-parent_folder_path = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')
-)
-sys.path.append(parent_folder_path)
-from db.models import VKUser, async_session  # noqa
+from middleware import BanMiddleware
 
 logging.getLogger('vkbottle').setLevel(logging.INFO)
 
 bot = Bot(api=api, labeler=labeler, state_dispenser=state_dispenser)
 bot.labeler.vbml_ignore_case = True
+
+bot.labeler.message_view.register_middleware(BanMiddleware)
 
 
 class UserStates(BaseStateGroup):
@@ -55,165 +49,111 @@ class AdminStates(BaseStateGroup):
     WAITING_USER_ID_TO_UNBAN = 'waiting_user_id_to_unban'
 
 
-async def is_user_banned(user_id: int) -> bool:
-    async with async_session() as session:
-        user = await session.execute(
-            select(VKUser).where(VKUser.user_id == user_id)
-        )
-        user = user.scalar_one_or_none()
-
-        if user and user.is_banned:
-            return True
-        return False
-
-
 @bot.on.private_message(lev='/admin')
 async def admin_start(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_start_handler(bot, message, AdminStates)
+    await admin_start_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.ADMIN_STATE)
 async def admin_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_handler(bot, message, AdminStates)
+    await admin_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.LINKS_STATE)
 async def links_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_links_handler(bot, message, AdminStates)
+    await admin_links_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_LINK)
 async def waiting_link(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await get_link(bot, message, AdminStates)
+    await get_link(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_LINK_NAME)
 async def waiting_link_name(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await get_link_name(bot, message, AdminStates)
+    await get_link_name(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_LINK_TYPE)
 async def waiting_link_type(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await get_link_type(bot, message, AdminStates)
+    await get_link_type(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_LINK_TO_ROLE)
 async def waiting_to_role(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await add_link(bot, message, AdminStates)
+    await add_link(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.DELETE_LINK)
 async def delete_link(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await delete_link_handler(bot, message, AdminStates)
+    await delete_link_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.PROMOCODES_STATE)
 async def promocodes_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_promocodes_handler(bot, message, AdminStates)
+    await admin_promocodes_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_PROMOCODE)
 async def waiting_promocode(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await get_promocode(bot, message, AdminStates)
+    await get_promocode(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_PROMOCODE_FILEPATH)
 async def waiting_promocode_filepath(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await add_promocode(bot, message, AdminStates)
+    await add_promocode(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.DELETE_PROMOCODE)
 async def delete_promocode(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await delete_promocode_handler(bot, message, AdminStates)
+    await delete_promocode_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(
     state=[AdminStates.UPLOAD_LINK_FILE, AdminStates.UPLOAD_PROMOCODE_FILE]
 )
 async def upload_file(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_upload_file_handler(bot, message, AdminStates)
+    await admin_upload_file_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.USERS_STATE)
 async def users_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await admin_users_handler(bot, message, AdminStates)
+    await admin_users_handler(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_USER_ID_TO_BAN)
 async def waiting_user_id_to_ban(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await ban_user(bot, message, AdminStates)
+    await ban_user(bot, message, AdminStates)
 
 
 @bot.on.private_message(state=AdminStates.WAITING_USER_ID_TO_UNBAN)
 async def waiting_user_id_to_unban(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await unban_user(bot, message, AdminStates)
+    await unban_user(bot, message, AdminStates)
 
 
 @bot.on.private_message(lev=['/start', 'Начать'])
 async def greeting(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await start_handler(bot, message, UserStates)
+    await start_handler(bot, message, UserStates)
 
 
 @bot.on.private_message(state=UserStates.ROLE_STATE)
 async def choose_role(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await role_handler(bot, message, UserStates)
+    await role_handler(bot, message, UserStates)
 
 
 @bot.on.private_message(state=UserStates.PARENT_STATE)
 async def parent_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await parent_handler(bot, message, UserStates)
+    await parent_handler(bot, message, UserStates)
 
 
 @bot.on.private_message(state=UserStates.FAQ_STATE)
 async def faq_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await faq_handler(bot, message, UserStates)
+    await faq_handler(bot, message, UserStates)
 
 
 @bot.on.private_message(state=UserStates.SPEECH_THERAPIST_STATE)
 async def speech_therapist_options(message: Message):
-    banned_user = await is_user_banned(message.from_id)
-    if not banned_user:
-        await speech_therapist_handler(bot, message, UserStates)
+    await speech_therapist_handler(bot, message, UserStates)
 
 
 if __name__ == '__main__':
